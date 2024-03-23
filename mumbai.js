7d519a08e8cc0e1f629e195661fb1ede4199b7d6
Archi Kumari <archikumari1012@gmail.com>
2024-03-23 16:00:35 +0530

    tour application

---
+++ mumbai.js
@@ -0,0 +1,11 @@
+/* Design Credit to: https://dribbble.com/shots/3553133-Mountains/attachments/790356 */
+
+$(document).ready(function() {
+	$('.carousel').flickity({
+		wrapAround: false,
+		freeScroll: true,
+		contain: true, 
+		prevNextButtons: false,
+		pageDots: false
+	});	
+})
